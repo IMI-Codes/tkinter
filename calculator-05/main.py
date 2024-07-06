444745d452a1e5f24e030e4eb32d36c0a23050cc
Manasseh <rougerapper@gmail.com>
2024-07-06 12:13:21 +0100

    working ont the calculator took a break

---
+++ calculator-05/main.py
@@ -0,0 +1,62 @@
+from tkinter import *
+
+gui = Tk()
+gui.title('Simple Calculator')
+gui.geometry('800x600')
+
+
+
+#function definitions
+
+def button_click(number):
+  current = userInput.get()
+  userInput.delete(0,END)
+  userInput.insert(0,str(current)+str(number))
+
+
+#input box
+userInput = Entry(gui,width=50,borderwidth=5)
+userInput.grid(row=0,column=0, columnspan=3, padx=5,pady=2)
+
+
+#creating buttons 
+button_1 = Button(gui,text=1, padx=40,pady=40,command=lambda: button_click(1))
+button_2 = Button(gui,text=2, padx=40,pady=40,command=lambda: button_click(2))
+button_3 = Button(gui,text=3, padx=40,pady=40,command=lambda: button_click(3))
+button_4 = Button(gui,text=4, padx=40,pady=40,command=lambda: button_click(4))
+button_5 = Button(gui,text=5, padx=40,pady=40,command=lambda: button_click(5))
+button_6 = Button(gui,text=6, padx=40,pady=40,command=lambda: button_click(6))
+button_7 = Button(gui,text=7, padx=40,pady=40,command=lambda: button_click(7))
+button_8 = Button(gui,text=8, padx=40,pady=40,command=lambda: button_click(8))
+button_9 = Button(gui,text=9, padx=40,pady=40,command=lambda: button_click(9))
+button_0 = Button(gui,text=0, padx=40,pady=40,command=lambda: button_click(0))
+
+add = Button(gui,text="+",padx=40,pady=40)
+equal = Button(gui,text="=",padx=40,pady=40)
+clear = Button(gui,text="CE",padx=40,pady=40)
+
+
+#puts the buttons on the screen
+#row 1
+clear.grid(row=1,column=1,padx=10,pady=10)
+add.grid(row=1,column=2,padx=10,pady=10)
+#row 2
+button_7.grid(row=2,column=0, padx=10,pady=10)
+button_8.grid(row=2,column=1, padx=10,pady=10)
+button_9.grid(row=2,column=2, padx=10,pady=10)
+
+#row 3 
+button_4.grid(row=3,column=0, padx=10,pady=10)
+button_5.grid(row=3,column=1, padx=10,pady=10)
+button_6.grid(row=3,column=2, padx=10,pady=10)
+
+#row 4
+button_1.grid(row=4,column=0, padx=10,pady=10)
+button_2.grid(row=4,column=1, padx=10,pady=10)
+button_3.grid(row=4,column=2, padx=10,pady=10)
+
+#row 5
+button_0.grid(row=5,column=1, padx=10,pady=10)
+equal.grid(row=5,column=2,padx=10,pady=10)
+
+gui.mainloop()
