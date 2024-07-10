5e57bdc0425679efff21b8af71d7b0780fb6f0d8
Manasseh <rougerapper@gmail.com>
2024-07-10 20:58:38 +0100

    started working with images

--- chapter06/main.py
+++ chapter06/main.py
@@ -1,10 +1,28 @@
 #using icons, images,exit buttons
 from tkinter import *
+from PIL import ImageTk,Image
 
 # the main display AKA root
 GUI = Tk()
 GUI.title('My App')
 GUI.geometry('900x700')
+# use this to change to default icon pass in the location of the icon to be displayed GUI.iconbitmap()
 
 
+
+
+
+
+
+
+myImage = ImageTk.PhotoImage(Image.open('pictures\7deb81b3c1be503144df67f61c1c4be9.jpg'))
+myLabel = Label(image=myImage)
+myLabel.pack()
+
+
+
+
+
+quitButton = Button(GUI,text="Exit",command=GUI.quit)
+quitButton.pack()
 GUI.mainloop()
---
+++ databaseGUI/main.py
@@ -0,0 +1,33 @@
+from tkinter import *
+
+GUI_MAIN = Tk()
+
+#FIRST NAME
+firstName = Entry()
+
+#MIDDLE NAME
+middleName = Entry()
+
+#LAST NAME
+lastName = Entry()
+
+#SEX
+sex = Listbox()
+
+#BIRTHDAY
+#birthDay = 
+
+#AGE 
+#add to compare the birthday entered and the age entered
+#age =
+
+#OTHERNAME
+otherName = Entry()
+
+#NICKNAME
+nickName = Entry() 
+
+
+
+
+GUI_MAIN.mainloop()
