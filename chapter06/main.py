1d973824fddd00647e2b7825191c2113e70c311f
Manasseh <rougerapper@gmail.com>
2024-07-18 18:41:32 +0100

    finally got a image to appear

--- chapter06/main.py
+++ chapter06/main.py
@@ -1,25 +1,11 @@
-#using icons, images,exit buttons
-from tkinter import *
-from PIL import ImageTk,Image
-# the main display AKA root
-GUI = Tk()
-GUI.title('My App')
-GUI.geometry('900x700')
-# use this to change to default icon pass in the location of the icon to be displayed GUI.iconbitmap()
+from customtkinter import *
+from PIL import ImageTk ,Image
 
 
-my_image = Image.open("7deb81b3c1be503144df67f61c1c4be9.jpg")
-img = ImageTk.PhotoImage(my_image)
-myLabel = Label(image= img)
+GUI = CTk()
+GUI.title('Images')
 
-
-
-
-
-
-
-
-
-quitButton = Button(GUI,text="Exit",command=GUI.quit)
-quitButton.pack()
+img = ImageTk.PhotoImage(Image.open("chapter06\wingsofFreedom.jpg"))
+myLabel = CTkLabel(image=img,master=GUI)
+myLabel.pack()
 GUI.mainloop()
