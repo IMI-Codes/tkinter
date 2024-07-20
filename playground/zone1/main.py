90e4b1169edd321aed533ec1f4db25746a20f059
Manasseh <rougerapper@gmail.com>
2024-07-20 23:37:21 +0100

    It was not pretty but we got it in

--- playground/zone1/main.py
+++ playground/zone1/main.py
@@ -5,17 +5,24 @@ GUI = CTk()
 GUI.title('Leveling Up')
 newPlayer = player()
 
+def setuserName():
+  userName = userInput.get()
+  if len(userName) == 0:
+    return
+  else:
+    newPlayer.setPlayerName(userName)
 #Components
+welcomeMsg = CTkLabel(master=GUI,text=f"Hello {newPlayer.playerName}",font=("Roboto",75))
 
-welcomMsg = CTkLabel(text=f"Hello {newPlayer.playerName}",master=GUI,font=('Roboto',75))
-
-userName = CTkLabel(text="Please Enter Your Name Below",master=GUI,font=('Roboto',50))
-
-userInput = CTkTextbox(master=GUI)
-submit = CTkButton(master=GUI,text="Home")
+#Getting userName
+nameRequest = CTkLabel(master=GUI,text="Please Enter your Name")
+userInput = CTkEntry(master=GUI,width=100)
+setName = CTkButton(master=GUI,command=setuserName,text="Set New Name")
 #Placing components on the screen
-welcomMsg.grid(row=0,column=0)
-userName.grid(row=1,column=1)
+welcomeMsg.grid(row=0,column=1)
+nameRequest.grid(row=1,column=0)
+userInput.grid(row=2,column=0)
+setName.grid(row=3,column=0)
 #myLabel.pack()
 GUI.mainloop()
 
